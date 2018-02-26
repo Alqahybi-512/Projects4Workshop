f03d27d288fdeb5bc363bba40cdc37c13ef74f5c
Ali Muhammad Q <salloy512@gmail.com>
2018-02-27 00:38:05 +0300

    some fixes and develops to look better

--- ATM solution/atm.py
+++ ATM solution/atm.py
@@ -6,24 +6,30 @@ class ATM:
     def withdraw(self, request):
         print("Welcome to " + str(self.bank_name))
         print("Current balance = " + str(self.balance))
-
-        allowed_papers = [100, 50, 10, 5, 1]
+        print "========================================="
 
         if request > self.balance:
             print "Sorry, Can't give you all this money !!"
+            print "========================================="
 
         elif request < 0:
             print "Must be more than 0 ,TRY more plz!"
+            print "========================================="
 
         else:
-            reminder = self.balance - request
+            self.balance -= request
+            self.process_request(request)
 
+    @staticmethod
+    def process_request(request):
+            allowed_papers = [100, 50, 10, 5, 1]
             for x in allowed_papers:
                 while request >= x:
                     print "Give:", x
                     request -= x
 
-            return reminder
+            print "========================================="
+            return request
 
 
 balance1 = 500
@@ -33,11 +39,9 @@ atm1 = ATM(balance1, "Smart Bank")
 atm2 = ATM(balance2, "Baraka Bank")
 
 atm1.withdraw(277)
-print
 atm1.withdraw(800)
-print
+
 atm2.withdraw(100)
-print
 atm2.withdraw(2000)
 
 
