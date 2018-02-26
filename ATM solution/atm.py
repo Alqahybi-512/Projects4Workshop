d7e239990ef17f0d14d9138f8dbb71e5565e613c
Ali Muhammad Q <salloy512@gmail.com>
2018-02-26 16:58:48 +0300

    Added a new solution

--- ATM solution/atm.py
+++ ATM solution/atm.py
@@ -1,25 +1,49 @@
-def atm(request,money):
-	reminder = money - request
-	allowed_papers = [100, 50, 10, 5, 1]
+class ATM:
+    def __init__(self, balance, bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
 
-	if request <= 0:
-		print "Must be more than 0 ,TRY more!"
-	elif request > money:
-		print "Sorry, There is no enough money,TRY less!"
-		print "Your Account value is " + str(money)
-	else :
-		print "Requested:",request
-		print "Your Balance is:" , reminder
-		
-		for x in allowed_papers:
-			while request >= x:
-				print "Give:", x
-				request -= x
+    def withdraw(self, request):
+        print("Welcome to " + str(self.bank_name))
+        print("Current balance = " + str(self.balance))
+
+        allowed_papers = [100, 50, 10, 5, 1]
+
+        if request > self.balance:
+            print "Sorry, Can't give you all this money !!"
+
+        elif request < 0:
+            print "Must be more than 0 ,TRY more plz!"
+
+        else:
+            reminder = self.balance - request
+
+            for x in allowed_papers:
+                while request >= x:
+                    print "Give:", x
+                    request -= x
+
+            return reminder
 
 
-atm(277,500)
-atm(1000,750)
-atm(500,232)
-atm(-500,1000)
+balance1 = 500
+balance2 = 1000
 
-######################################
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+print
+atm1.withdraw(800)
+print
+atm2.withdraw(100)
+print
+atm2.withdraw(2000)
+
+
+###################################################
+
+
+
+
+
